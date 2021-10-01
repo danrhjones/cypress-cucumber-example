6eab0cb48c6c7664ff43d7e1df9a6cb933ab4c4e
Dan Jones <dan.rh.jones@gmail.com>
2021-10-01 14:38:10 +0100

    add another example

--- cypress/integration/common/signInSteps.js
+++ cypress/integration/common/signInSteps.js
@@ -1,4 +1,4 @@
-import { Given } from "cypress-cucumber-preprocessor/steps";
+import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
 import SignIn from "../../elements/pages/SignInPage";
 import AddPost from "../../elements/pages/addPost";
 
@@ -6,4 +6,13 @@ Given("I have saved a post", () => {
   SignIn.login();
 
   AddPost.newPost();
+});
+
+When(/^I attempt to view the revisions$/, () => {
+  // Todo open the revisions bit
+
+});
+
+Then(/^I see that there is no content to process$/, () => {
+// todo
 });
